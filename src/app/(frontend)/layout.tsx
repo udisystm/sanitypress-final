ed0291e7e289735fcf19fedc576694677c824e33
dev-shehzad <devshehzad@gmail.com>
2025-01-19 21:21:53 +0500

    gpt

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5RL6HXL2');`,
+            })(window,document,'script','dataLayer','GTM-N2PZMLHB');`,
           }}
         />
       </head>
@@ -41,7 +41,7 @@ export default async function RootLayout({
         {/* Google Tag Manager NoScript for Body */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5RL6HXL2"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2PZMLHB"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
